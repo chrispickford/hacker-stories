dc13bda222f8deaefb3d39aa0fa57b80cb674588
Chris Pickford <chris@luminousuk.com>
2021-07-06 10:36:14 +0100

    Reusable React Component

--- src/App.js
+++ src/App.js
@@ -43,7 +43,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
       
       <hr />
 
@@ -52,14 +57,14 @@ const App = () => {
   );
 }
 
-const Search = ({search, onSearch}) => (
+const InputWithLabel = ({id, label, value, type = 'text', onInputChange}) => (
   <>
-    <label htmlFor="search">Search: </label>
+    <label htmlFor={id}>{label}</label>
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
 );
