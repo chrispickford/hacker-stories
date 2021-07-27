03aec16a7740a2c8e6e5221cf2c8726e38709863
Chris Pickford <chris@luminousuk.com>
2021-07-27 17:45:07 +0100

    React Asynchronous Data

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ const initialStories = [
   }
 ];
 
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { stories: initialStories } }),
+    2000
+    )
+  );
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState);
@@ -32,7 +39,13 @@ const useSemiPersistentState = (key, initialState) => {
 
 const App = () => {
   
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
